a33a9bd90afed3c0ba29ccfc2a784e0b71403edd
pofider <jan.blaha@hotmail.com>
2017-12-07 22:38:48 +0100

    revert previous accidental commit

--- Program.cs
+++ Program.cs
@@ -11,65 +11,27 @@ namespace ConsoleApp
     class Program
     {
         static void Main(string[] args)
-        {         
+        {
             Console.WriteLine("Initializing local jsreport.exe utility");
             var rs = new LocalReporting()
+                .RunInDirectory(Path.Combine(Directory.GetCurrentDirectory(), "jsreport"))
                 .KillRunningJsReportProcesses()
                 .UseBinary(JsReportBinary.GetBinary())
                 .Configure(cfg => cfg.AllowLocalFilesAccess().FileSystemStore().BaseUrlAsWorkingDirectory())
-                .AsUtility()
+                .AsWebServer()
                 .Create();
-            //.AsWebServer()
-            //.RedirectOutputToConsole()
 
+            rs.StartAsync().Wait();
+            Console.ReadKey();
 
-            /*            rs.StartAsync().Wait();
-                        Console.ReadLine();
-                        rs.KillAsync().Wait();
-                        return;*/
-
-            /* Console.WriteLine("Rendering localy stored template jsreport/data/templates/Invoice into invoice.pdf");
-             var invoiceReport = rs.RenderByNameAsync("Invoice", InvoiceData).Result;
-
-             var logs = "";            
-             long startTime = invoiceReport.Meta.Logs.First().Timestamp.Ticks;
-             foreach (var l in invoiceReport.Meta.Logs)
-             {               
-                 logs += $"+{Math.Round((double)(l.Timestamp.Ticks - startTime)/1000)} {l.Message}\n";
-             }            
-
-             invoiceReport.Content.CopyTo(File.OpenWrite("invoice.pdf"));*/
+            Console.WriteLine("Rendering localy stored template jsreport/data/templates/Invoice into invoice.pdf");
+            var invoiceReport = rs.RenderByNameAsync("Invoice", InvoiceData).Result;
+            invoiceReport.Content.CopyTo(File.OpenWrite("invoice.pdf"));
 
             Console.WriteLine("Rendering custom report fully described through the request object into customReport.pdf");
-            var customReport = rs.RenderAsync(new RenderRequest()
-            {
-                Template = new Template()
-                {
-                    Content = File.ReadAllText("test.html"),
-                    Engine = Engine.None,
-                    Recipe = Recipe.PhantomPdf
-                }
-            }).Result;
-
-            customReport = rs.RenderAsync(new RenderRequest()
-            {
-                Template = new Template()
-                {
-                    Content = /*File.ReadAllText("test.html")*/"Hello",
-                    Engine = Engine.None,
-                    Recipe = Recipe.PhantomPdf
-                }
-            }).Result;
-
-            var logs = "";
-            long startTime = customReport.Meta.Logs.First().Timestamp.Ticks;
-            foreach (var l in customReport.Meta.Logs)
-            {
-                logs += $"+{Math.Round((double)(l.Timestamp.Ticks - startTime) / 1000)} {l.Message}\n";
-            }
-
+            var customReport = rs.RenderAsync(CustomRenderRequest).Result;
             customReport.Content.CopyTo(File.OpenWrite("customReport.pdf"));
-        }   
+        }
 
         private static RenderRequest CustomRenderRequest = new RenderRequest()
         {
